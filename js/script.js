7e06b29b4d8fa46668ed76aadb69be1d871a3a6f
Mohana Krishnan <smkrishna96@gmail.com>
2017-04-22 03:31:55 +0530

    All tabs provided

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 function about(){
-	disp = $('#displayContent').text();
 	window.alert("disp");
+	$.get('content/about.txt', function(data) {
+		window.alert(data);
+		$('.displayContent').html(data);
+	}, 'text');
+}
+
+function title(){
+	window.alert("disp");
+	$.get('content/about.txt', function(data) {
+		window.alert(data);
+		$('.displayContent').html(data);
+	}, 'text');
+}
+
+function change(file){
+	file = "content/"+file+".html";
+	$.get(file, function(data) {
+		$('.displayContent').html(data);
+	}, 'text');
 }
