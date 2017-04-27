16bc255100540f84a865a9a6e7b68943679b6d3d
Ajmal Hussain HY <ajmalhussainhy@gmail.com>
2017-04-27 10:24:02 +0530

    Changes and Bug fixes
    + Added reg link in hunt talent tab
    + Added anchor tag styles
    + Changed colors tab bg color
    - Removed prisma webfont
    - Removed card custom styles

--- js/cc.js
+++ js/cc.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		anchors:['home', 'about', 'students','vips', 'freelanc', 'hunt', 'course', 'college', 'upeve', 'free'],
-		sectionsColor : ['#3949AB', '#FFB300', '#1BAEE4', '#F35F72', '#435196', '#00796B', '#795548', '#607D8B', '#673AB7', '#b71c1c'],
+		sectionsColor : ['#3949AB', '#FFB300', '#1BAEE4', '#F35F72', '#435196', '#00796B', '#CDDC39', '#607D8B', '#673AB7', '#b71c1c'],
 		menu: '#menu',
         css3: true,
         easing: 'easeInOutCubic',
